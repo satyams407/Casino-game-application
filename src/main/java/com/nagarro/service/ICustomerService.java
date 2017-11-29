88c851a76bdae52c3019c4e7b62c165278e774ad
Satyam Sehgal <satyams407@outlook.com>
2017-11-29 11:59:42 +0530

    Update ICustomerService.java

--- src/main/java/com/nagarro/service/ICustomerService.java
+++ src/main/java/com/nagarro/service/ICustomerService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import com.nagarro.dao.Customer;
 
 public interface ICustomerService {
-
 	public List<Customer> getAllCustomers();
 	
 	public void addCustomer(Customer data);
+	
+	public Customer getCustomer(Integer id);
+	
+	public void updateCustomerBalance(Integer id, Customer data);
 }
