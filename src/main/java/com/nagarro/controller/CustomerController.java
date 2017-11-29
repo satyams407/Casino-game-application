bfe27355a50b4d5f37ee7dc4b88f789c670f1010
Satyam Sehgal <satyams407@outlook.com>
2017-11-29 12:00:59 +0530

    Update CustomerController.java
    added some controllers for post and put Rest Apis

--- src/main/java/com/nagarro/controller/CustomerController.java
+++ src/main/java/com/nagarro/controller/CustomerController.java
@@ -3,27 +3,40 @@ package com.nagarro.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.nagarro.dao.Customer;
-import com.nagarro.service.CustomerService;
+import com.nagarro.service.ICustomerService;
 
+@CrossOrigin
 @RestController
-public class CustomerController  {
+public class CustomerController {
 
 	@Autowired
-	private CustomerService customerService;
-	
-	@RequestMapping("/users")   
-	public List<Customer> getAllCustomers(){
-		return customerService.getAllCustomers();	
+	private ICustomerService customerService;
+
+	@GetMapping(path = "/users", produces = "application/json")
+	public List<Customer> getAllCustomers() {
+		return customerService.getAllCustomers();
 	}
-	
-	@RequestMapping(method=RequestMethod.POST,value="/users")
+
+	@GetMapping(path = "/users/{id}", produces = "application/json")
+	public Customer getCustomer(@PathVariable Integer id) {
+		return customerService.getCustomer(id);
+	}
+
+	@PostMapping(value = "/users")
 	public void addCustomer(@RequestBody Customer customer) {
 		customerService.addCustomer(customer);
 	}
+
+	@PostMapping(value = "/users/{id}")
+	public void updateCustomerBalance(@RequestBody Customer customer, @PathVariable Integer id) {
+		customerService.updateCustomerBalance(id, customer);
+	}
+
 }
