17bbd66862da65de7634372ccff7fb3c87854417
Satyam Sehgal <satyams407@outlook.com>
2017-11-29 11:59:21 +0530

    Update CustomerService.java
    added some more business logic

--- src/main/java/com/nagarro/service/CustomerService.java
+++ src/main/java/com/nagarro/service/CustomerService.java
@@ -2,15 +2,17 @@ package com.nagarro.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.nagarro.dao.Customer;
 import com.nagarro.repository.ICustomerRepository;
 
 @Service
-public class CustomerService {
+public class CustomerService implements ICustomerService {
 
 	@Autowired
 	private ICustomerRepository customerRepository;
@@ -21,7 +23,21 @@ public class CustomerService {
 		return users;
 	}
 
+	public Customer getCustomer(Integer id) {
+		return customerRepository.findOne(id);
+	}
+
 	public void addCustomer(Customer data) {
+		data.setAccountBalance(500.00);
+		String uniqueKey = UUID.randomUUID().toString();
+		data.setUserId(uniqueKey);
 		customerRepository.save(data);
 	}
+
+	public void updateCustomerBalance(Integer id, Customer data) {
+		Customer customer = customerRepository.findOne(id);
+		customer.setAccountBalance(data.getAccountBalance());
+		customerRepository.save(customer);
+	}
+
 }
