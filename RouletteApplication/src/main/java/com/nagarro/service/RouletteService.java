e7c64f039268219f7bbc5fa6c419b8e260c542a1
Satyam Sehgal <satyams407@outlook.com>
2017-12-06 14:17:00 +0530

    Update RouletteService.java

--- RouletteApplication/src/main/java/com/nagarro/service/RouletteService.java
+++ RouletteApplication/src/main/java/com/nagarro/service/RouletteService.java
@@ -1,6 +1,10 @@
 package com.nagarro.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,12 +26,6 @@ public class RouletteService implements IRouletteService {
 		return getResponse.getBody().getObject();
 	}
 
-	// nextInt is normally exclusive of the top value,
-	// so add 1 to make it inclusive
-	public int getRandomNumber() {
-		return ThreadLocalRandom.current().nextInt(0, 37);
-	}
-
 	public int getBlockedAmount(BetAmount betAmount) {
 		int sum = 0;
 		sum += betAmount.getFirst12();
@@ -41,9 +39,59 @@ public class RouletteService implements IRouletteService {
 		return sum;
 	}
 
-	public int gameResult(BetAmount bet) {
+	public int generateRandomNumber() {
+		ArrayList<Long> randomNumbers = new ArrayList<Long>();
+		int randomNumber = ThreadLocalRandom.current().nextInt(1, 11);
+
+		// generating 20 random number of length equals to above generated
+		// random number
+		for (int i = 0; i < 20; i++) {
+			long randomNew = ThreadLocalRandom.current().nextLong((long) Math.pow(10, randomNumber - 1),
+					(long) Math.pow(10, randomNumber));
+			randomNumbers.add(randomNew);
+		}
+
+		Collections.sort(randomNumbers);
+		long firstListSum = 0, secondListSum = 0;
+		int i = 0, firstListIndex = 0, secondListIndex = 0;
+
+		/*
+		 * for (i = 0; i < randomNumbers.size(); i += 2) { if (flag == 1) {
+		 * firstListSum += randomNumbers.get(i); secondListSum +=
+		 * randomNumbers.get(i + 1); flag--; } else { secondListSum +=
+		 * randomNumbers.get(i); firstListSum += randomNumbers.get(i + 1);
+		 * flag++; } }
+		 */
+
+		for (i = randomNumbers.size() - 1; i > 0; i--) {
+			if (firstListIndex < 10 && secondListIndex < 10) {
+
+				if (firstListSum >= secondListSum) {
+					firstListSum += randomNumbers.get(i - 1);
+					secondListSum += randomNumbers.get(i);
+					firstListIndex++;
+					secondListIndex++;
+				} else {
+					firstListSum += randomNumbers.get(i);
+					secondListSum += randomNumbers.get(i - 1);
+					firstListIndex++;
+					secondListIndex++;
+				}
+			} else if (secondListIndex >= 10) {
+				firstListSum += randomNumbers.get(i);
+				firstListIndex++;
+			} else {
+				secondListSum += randomNumbers.get(i);
+				secondListIndex++;
+			}
+		}
+		return (int) ((Math.abs(firstListSum - secondListSum)) % 37);
+	}
+
+	public int gameResult(BetAmount bet, HttpSession session) {
 		int result = 0;
-		int randomNum = getRandomNumber();
+		int randomNum = generateRandomNumber();
+		session.setAttribute("randomNumber", randomNum);
 		if (1 <= randomNum && randomNum <= 12)
 			result += bet.getFirst12() * 1.5;
 		if (13 <= randomNum && randomNum <= 24)
@@ -56,10 +104,11 @@ public class RouletteService implements IRouletteService {
 			result += bet.getOneToEighteen() * 1.25;
 		if (19 <= randomNum && randomNum <= 36)
 			result += bet.getNineteenToThirySix() * 1.25;
-		if (randomNum % 2 == 0)
+		if (randomNum % 2 == 0 && randomNum != 0)
 			result += bet.getEven() * 1.25;
 		if (randomNum % 2 == 1)
 			result += bet.getOdd() * 1.25;
+
 		return result;
 	}
 
